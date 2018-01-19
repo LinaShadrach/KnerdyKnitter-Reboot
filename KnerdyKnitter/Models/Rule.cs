1946c074f05b8c63024d3f9228d9dcb5f1f9a942
LinaShadrach <lina.shadrach@gmail.com>
2018-01-19 15:24:40 -0800

    create skeleton of Rule class

---
+++ KnerdyKnitter/Models/Rule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace KnerdyKnitter.Models
+{
+    public static class Rule
+    {
+        public static int[,] BaseCombos =
+        {   { 1, 1, 1 },
+            { 1, 1, 0 },
+            { 1, 0, 1 },
+            { 1, 0, 0 },
+            { 0, 1, 1 },
+            { 0, 1, 0 },
+            { 0, 0 ,1 },
+            { 0, 0, 0 }
+        };
+        public static int[] GetRuleAsIntArray(int ruleAsDecimal){
+            int[] ruleAsIntArray = new int[8];
+            int count = 0;
+            foreach(char bit in (Convert.ToString(ruleAsDecimal, 2)))
+            {
+                ruleAsIntArray[count] = bit;
+                count++;
+            }
+            return ruleAsIntArray;
+        }
+    }
+}
