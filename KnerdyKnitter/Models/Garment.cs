0062ae4985d4525921ed5eef4f032a33fe822b79
LinaShadrach <lina.shadrach@gmail.com>
2018-01-19 16:05:01 -0800

    create IGarment

---
+++ KnerdyKnitter/Models/Garment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace KnerdyKnitter.Models
+{
+    public interface IGarment
+    {
+        int Id { get; set; }
+        string type { get; set; }
+        int[] rule { get; set; }
+    }
+}
