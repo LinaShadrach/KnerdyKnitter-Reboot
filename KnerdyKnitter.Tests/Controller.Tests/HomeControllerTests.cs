47aa364f4dc9a3a34973cf2bcd4ac0950ee8b6e8
LinaShadrach <lina.shadrach@gmail.com>
2018-02-15 13:38:43 -0800

    add test for HomeContoller/Index()

---
+++ KnerdyKnitter.Tests/Controller.Tests/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using KnerdyKnitter.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KnerdyKnitter.Tests.Controller.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectType_True()
+        {
+            ViewResult result = new HomeController().Index() as ViewResult;
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
