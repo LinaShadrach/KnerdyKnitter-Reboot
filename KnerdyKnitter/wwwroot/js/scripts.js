32d8555da31c13d653542163c3972412e6307fda
LinaShadrach <lina.shadrach@gmail.com>
2018-02-20 21:35:03 -0800

    add ability to change dimensions of scarf on create

--- KnerdyKnitter/wwwroot/js/scripts.js
+++ KnerdyKnitter/wwwroot/js/scripts.js
@@ -9,6 +9,21 @@ function changeColors(){
   changeSecondaryColor();
 }
 
+function changeDimensions(){
+  drawScarf($("#rule").val());
+}
+
+function getRule(){
+  return $("#rule").val();
+}
+
+function getWidth(){
+  return $("#width").val();
+}
+function getLength(){
+  return $("#length").val();
+}
+
 function changePrimaryColor() {
   var primaryColor = $("#primaryColor").val();
   $(".primary").css("fill", primaryColor);
@@ -19,30 +34,32 @@ function changeSecondaryColor() {
   $(".secondary").css("fill", secondaryColor);
 }
 
-function changeRule(rule) {
-  rule = changeRuleToBinaryArray(rule);
+function changeRule() {
+  var rule = changeRuleToBinaryArray(getRule());
   drawScarf(rule);
 }
 
 function drawScarf(rule) {
-  var scarfSvg = "<svg class='scarf' width='200' height='2000'>";
-  var baseRow = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0];
-  var creation = createScarfBinary(baseRow, rule);
+  var width = parseInt(getWidth()) * 20;
+  var height = parseInt(getLength()) * 20;
+  var scarfSvg = "<svg class='scarf' width='" + width + "' height='" + height + "'>";
+  var creation = createScarfBinary(rule);
   var x = 0;
   var y = 0;
+  var sideSize = 20;
   creation.forEach(function(row) {
     row.forEach(function(cell) {
-      scarfSvg += "<rect id='' x='"+x+"' y='"+y+"' width='20' height='20' class= cell";
+      scarfSvg += "<rect id='' x='"+x+"' y='"+y+"' width='" + sideSize + "' height='" + sideSize + "' class='cell ";
       if(cell===0){
         scarfSvg += "primary'/>";
       }
       else {
         scarfSvg += "secondary'/>";
       }
-      x+=20;
+      x+=sideSize;
     });
     x=0;
-    y+=20;
+    y+=sideSize;
   });
   scarfSvg += "</svg>";
   $(".scarf-container").empty();
@@ -62,16 +79,18 @@ function changeRuleToBinaryArray(rule) {
   return rule;
 }
 
-function createScarfBinary(baseRow, rule) {
+function createScarfBinary(rule) {
+  var length = getLength();
   var creation = [[]];
   var column = 0;
+  var baseRow = createBaseRow();
   baseRow.forEach(function(cell) {
     creation[0].push(cell);
   })
-  for(var row=1; row<100; row++) {
+  for(var row = 1; row < length; row++) {
     creation[row]=[];
-    for(var column=0; column<baseRow.length; column++) {
-      if(column===0){
+    for(var column = 0; column < baseRow.length; column++) {
+      if(column === 0){
         creation[row].push(getCell([creation[row-1][baseRow.length - 1], creation[row-1][column], creation[row-1][column+1]], rule));
       }
       else if (column===baseRow.length-1) {
@@ -85,8 +104,23 @@ function createScarfBinary(baseRow, rule) {
   return creation;
 }
 
-function getCell(parentCells, rule) {
+function createBaseRow() {
+  var baseRow = [];
+  var width = getWidth();
+  for(var i = 0; i < width; i++) {
+    if(i === Math.round(width/2)) {
+      baseRow.push(1);
+    }
+    else {
+      baseRow.push(0);
+    }
+  }
+  return baseRow;
+}
+function getCell(parentCells) {
   var position = parseInt(parentCells.join(""), 2);
-  var reversedRule = rule.slice().reverse();
+  var rule = getRule();
+  var binaryRule = changeRuleToBinaryArray(rule);
+  var reversedRule = binaryRule.slice(0, binaryRule.length).reverse();
   return parseInt(reversedRule[position]);
 }
